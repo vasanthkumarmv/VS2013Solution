154fe0312d77355117cbbceb6438cdf8c0bccbbe
radhakrishnanmv <radhakrishnan@magnusvista.com>
2018-09-12 15:40:39 +0530

    add solution to github

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string siteUrl = "https://kartooz2000hotmail.sharepoint.com";
+            string userName = "azureadmin@kartooz2000hotmail.onmicrosoft.com";
+            string passWord = "IndMv123$";
+            ClientContext clientContext = null;
+            clientContext = new ClientContext(siteUrl);
+            var pwd = new SecureString();
+            foreach (char c in passWord.ToCharArray()) pwd.AppendChar(c);
+            clientContext.Credentials = new SharePointOnlineCredentials(userName, pwd);
+            Web web=clientContext.Web;
+            clientContext.Load(web.RoleDefinitions, roledefinitions => roledefinitions.Include(thisRole => thisRole.Name, thisRole => thisRole.Id));
+            clientContext.ExecuteQuery();
+        }
+    }
+}
